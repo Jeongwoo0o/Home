110ed77c0da23f028db2323a32e63c604459199d
jeongwoo <dkthnt06@gmail.com>
2024-03-12 10:47:27 +0900

    Test1

--- bOOP/src/e_static/Book.java
+++ bOOP/src/e_static/Book.java
@@ -1,8 +1,16 @@
 package e_static;
 
+/*
+ *  static
+ *      - 각 객체들끼리 공유
+ *      - 메모리에 단 1개 생성
+ * 
+ *      - 객체 생성보다 먼저 생성
+ */
+
 public class Book {
 
-    int count = 0;
+    static int count = 0;
 
     Book(){
         count++;
--- bOOP/src/e_static/Main.java
+++ bOOP/src/e_static/Main.java
@@ -2,11 +2,13 @@ package e_static;
 
 public class Main {
 
-    Book b1 = new Book();
-    Book b2 = new Book();
-    Book b3 = new Book();
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        Book b2 = new Book();
+        Book b3 = new Book();
 
-    System.out.println("책 개수 : " + b1.count);
-    System.out.println("책 개수 : " + b2.count);
-    System.out.println("책 개수 : " + b3.count);
+        System.out.println("책 개수 : " + b1.count);
+        System.out.println("책 개수 : " + b2.count);
+        System.out.println("책 개수 : " + b3.count);
+    }
 }
